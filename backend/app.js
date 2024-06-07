3097fb5da7d2aa4e2d7e4407088e22f65a67f12f
Garvit <garvitg0108@gmail.com>
2024-06-07 21:05:34 +0530

    new cors fix

--- backend/app.js
+++ backend/app.js
@@ -20,36 +20,38 @@ const propertyRoute = require('./routes/property')
 const errorHandler = require('./middleware/errorHandler')
 const notFound = require('./middleware/not-found')
 
-app.use('/uploads',express.static('uploads'))
-
-// app.use('/api/v1',testRoute);
-app.use('/api/v1',authRoute)
-app.use('/api/v1/property',propertyRoute)
-
-app.use(notFound)
-app.use(errorHandler)
-
 app.use(cors({
     origin: ["https://real-estate-navigator-noxxb5g1s-garvit-guptas-projects.vercel.app/", "http://localhost:3000"], // Allow requests from these origins
     methods: ["GET", "POST", "PATCH", "DELETE"], // Allowed methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
     credentials: true // Allow cookies to be sent with requests
-  }));
-  
+}));
+
+app.use('/uploads', express.static('uploads'))
+
+// app.use('/api/v1',testRoute);
+app.use('/api/v1', authRoute)
+app.use('/api/v1/property', propertyRoute)
+
+app.use(notFound)
+app.use(errorHandler)
+
+
+
 
 app.set('trust proxy', 1);
 app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
+    rateLimiter({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100, // limit each IP to 100 requests per windowMs
+    })
 );
 app.use(express.json());
 app.use(helmet());
 
 app.use(xss());
 
-const start = async() => {
+const start = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("Database connected ")
